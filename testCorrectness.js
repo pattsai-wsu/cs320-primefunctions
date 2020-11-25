77907a539055c988fb69cfbfda997ab0f64de592
Patrick Tsai <tsai.patrick.82@gmail.com>
2020-11-25 15:09:29 -0800

    finished cumulativeSum tests

--- testCorrectness.js
+++ testCorrectness.js
@@ -15,4 +15,18 @@ describe('primefunctions', function () {
       chai.expect(primeGen(20)).to.have.lengthOf(8);
     });
   });
+  describe('cumulativeSum()', function () {
+    it('cumulativeSum([1, 2, 3, 4]) should return [ 1, 3, 6, 10 ]', function () {
+      chai.expect(cumulativeSum([1, 2, 3, 4])).to.include.members([1, 3, 6, 10]);
+    });
+    it('cumulativeSum([1, 2, 3, 4]) should be length 4', function () {
+      chai.expect(cumulativeSum([1, 2, 3, 4])).to.have.lengthOf(4);
+    });
+    it('cumulativeSum([10, 11, 12, 13, 14]) should return [10, 21, 33, 46, 60]', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.include.members([10, 21, 33, 46, 60]);
+    });
+    it('cumulativeSum([10, 11, 12, 13, 14]) should be length 5', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.have.lengthOf(5);
+    });
+  });
 });
