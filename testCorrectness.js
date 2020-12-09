70147a7c8bf01fe67965c78efd7a2d21b23ec60e
Patrick Tsai <tsai.patrick.82@gmail.com>
2020-12-09 15:55:15 -0800

    Update testCorrectness.js

--- testCorrectness.js
+++ testCorrectness.js
@@ -4,19 +4,17 @@ describe('primefunctions', function () {
   describe('primeGen()', function () {
     it('primeGen(10) = [ 2, 3, 5, 7 ]', function () {
       chai.expect(primeGen(10)).to.deep.equal([2, 3, 5, 7]);
-      // chai.expect(primeGen(10)).to.include.members([2, 3, 5, 7]);
     });
     it('primeGen(20) = [ 2, 3, 5, 7, 11, 13, 17, 19 ]', function () {
       chai.expect(primeGen(20)).to.deep.equal([2, 3, 5, 7, 11, 13, 17, 19]);
-      // chai.expect(primeGen(20)).to.include.members([2, 3, 5, 7, 11, 13, 17, 19]);
     });
   });
   describe('cumulativeSum()', function () {
     it('cumulativeSum([1, 2, 3, 4]) = [ 1, 3, 6, 10 ]', function () {
-      chai.expect(cumulativeSum([1, 2, 3, 4])).to.include.members([1, 3, 6, 10]);
+      chai.expect(cumulativeSum([1, 2, 3, 4])).to.deep.equal([1, 3, 6, 10]);
     });
     it('cumulativeSum([10, 11, 12, 13, 14]) = [10, 21, 33, 46, 60]', function () {
-      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.include.members([10, 21, 33, 46, 60]);
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.deep.equal([10, 21, 33, 46, 60]);
     });
   });
   describe('maxPrimeSum()', function () {
